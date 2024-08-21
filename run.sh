a5586bb5e249e7ab10f49b1eb22ba9790a9509a1
Anju Chhetri <57993069+anju-chhetri@users.noreply.github.com>
2024-08-22 00:54:33 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export nnUNet_raw="/scratch/achhetri/data/nnUnetD4/nnUNet_raw"
 export nnUNet_preprocessed="/scratch/achhetri/data/nnUnetD4/preprocessed"
 export nnUNet_results="/scratch/achhetri/experimentalResults/nnUNetD4"
-nnUNetv2_train 17 3d_fullres 0 -p nnUNetResEncUNetMPlans --val --npz
+nnUNetv2_train 17 3d_fullres 0 -p nnUNetResEncUNetMPlans --npz
